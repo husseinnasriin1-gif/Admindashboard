5bf430d17964e4d16cb1528c747c2d67e3b9faed
Nasriin <husseinnasriin1@gmail.com>
2026-08-13 16:29:06 +0300

    changes

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -17,7 +17,7 @@ function Login() {
     setSuccessMessage("");
 
     try {
-      const response = await fetch("http://localhost:8080/login", {
+      const response = await fetch("https://admindashboard-9qfv.vercel.app/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
